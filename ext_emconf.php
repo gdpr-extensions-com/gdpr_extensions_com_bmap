c7556f690a644cd68d3b6fade5541d5d92bcaebb
gdpr-extensions-com <support@gdpr-extensions.com>
2024-06-04 17:21:33 +0500

    emconf updated

--- ext_emconf.php
+++ ext_emconf.php
@@ -6,12 +6,12 @@ $EM_CONF[$_EXTKEY] = [
     'category' => 'plugin',
     'author' => '',
     'author_email' => '',
-    'state' => 'alpha',
+    'state' => 'stable',
     'clearCacheOnLoad' => 0,
-    'version' => '1.0.0',
+    'version' => '1.0.1',
     'constraints' => [
         'depends' => [
-            'typo3' => '11.5.0-11.5.99',
+            'typo3' => '11.5.0-12.4.99',
         ],
         'conflicts' => [],
         'suggests' => [],
